52325080ae13fdd2192a1f56614480e72a260c5c
EnriqueGS88 <enrique.gzs@gmail.com>
2022-08-26 10:54:51 +0200

    added networks file

---
+++ src/networks.js
@@ -0,0 +1,30 @@
+export const networkParams = {
+    "0x63564c40": {
+        chainId: "0x63564c40",
+        rpcUrls: ["https://api.harmony.one"],
+        chainName: "Harmony Mainnet",
+        nativeCurrency: { name: "ONE", decimals: 18, symbol: "ONE" },
+        blockExplorerUrls: ["https://explorer.harmony.one"],
+        iconUrls: ["https://harmonynews.one/wp-content/uploads/2019/11/slfdjs.png"]
+      },
+      "0xa4ec": {
+        chainId: "0xa4ec",
+        rpcUrls: ["https://forno.celo.org"],
+        chainName: "Celo Mainnet",
+        nativeCurrency: { name: "CELO", decimals: 18, symbol: "CELO" },
+        blockExplorerUrl: ["https://explorer.celo.org"],
+        iconUrls: [
+          "https://celo.org/images/marketplace-icons/icon-celo-CELO-color-f.svg"
+        ]
+      },
+      "0x89": {
+        chainId: "0x89",
+        rpcUrls: ["https://polygon-rpc.com/"],
+        chainName: "Polygon Mainnet",
+        nativeCurrency: { name: "MATIC", decimals: 18, symbol: "MATIC" },
+        blockExplorerUrl: ["https://polygonscan.com/"],
+        iconUrls: [
+          "https://polygonscan.com/images/logo-white.svg?v=0.0.2"
+        ]
+      }
+};
