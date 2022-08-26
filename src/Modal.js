48fbd817f5976f7e5df8dc426021f18632c4c1d9
EnriqueGS88 <enrique.gzs@gmail.com>
2022-08-26 10:48:29 +0200

    created modal component

---
+++ src/Modal.js
@@ -0,0 +1,104 @@
+import {
+    VStack,
+    HStack,
+    Modal,
+    ModalOverlay,
+    ModalContent,
+    ModalHeader,
+    ModalBody,
+    ModalCloseButton,
+    Button,
+    Text
+} from "@chakra-ui/react";
+import { Image } from "@chakra-ui/react";
+import { useWeb3React } from "@web3-react/core";
+import { connectors } from "./connectors";
+
+import React from 'react'
+
+function SelectWalletModal( {isOpen, closeModal }) {
+  
+    const { activate } = useWeb3React();
+  
+    return (
+        
+        <Modal isOpen{ isOpen } onClose={ closeModal } isCentered>
+            <ModalOverlay />
+            <ModalContent w="300px">
+                <ModalHeader>Select Wallet</ModalHeader>
+                <ModalCloseButton 
+                    _focus={ {
+                        boxShadow: "none"
+                    }}
+                />
+                <ModalBody paddingBottom="1.5rem">
+                    <VStack>
+                        <Button
+                            variant="outlie"
+                            onClick={ () => {
+                                activate( connectors.coinbaseWallet);
+                                closeModal();
+                            }}
+                            w="100%"
+                        >
+                            <HStack w="100%" justifyContent="center">
+                                <Image
+                                    src="/cbw.png"
+                                    alt="Coinbase Wallet Logo"
+                                    width={ 25 }
+                                    heigth={ 25 }
+                                    borderRadius="3px"
+                                />
+                                <Text>Coinbase Wallet</Text>
+                            </HStack>
+                        </Button>
+                        
+
+                        <Button
+                            variant="outlie"
+                            onClick={ () => {
+                                activate( connectors.coinbaseWallet);
+                                closeModal();
+                            }}
+                            w="100%"
+                        >
+                            <HStack w="100%" justifyContent="center">
+                                <Image
+                                    src="/cbw.png"
+                                    alt="Wallet Connect Logo"
+                                    width={ 25 }
+                                    heigth={ 25 }
+                                    borderRadius="3px"
+                                />
+                                <Text>Wallet Connect</Text>
+                            </HStack>
+                        </Button>
+
+
+                        <Button
+                            variant="outlie"
+                            onClick={ () => {
+                                activate( connectors.coinbaseWallet);
+                                closeModal();
+                            }}
+                            w="100%"
+                        >
+                            <HStack w="100%" justifyContent="center">
+                                <Image
+                                    src="/cbw.png"
+                                    alt="Metamask Logo"
+                                    width={ 25 }
+                                    heigth={ 25 }
+                                    borderRadius="3px"
+                                />
+                                <Text>Metamask</Text>
+                            </HStack>
+                        </Button>
+                    </VStack>
+                </ModalBody>
+            </ModalContent>
+        </Modal>
+  );
+}
+
+export default SelectWalletModal
