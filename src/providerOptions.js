32119a06f2eafb3fb3336f5348df03632668cc57
EnriqueGS88 <enrique.gzs@gmail.com>
2022-08-27 23:57:10 +0200

    replaced dotenv with native react_app prefix

--- src/providerOptions.js
+++ src/providerOptions.js
@@ -1,19 +1,19 @@
 import WalletConnect from "@walletconnect/web3-provider";
 import CoinbaseWalletSDK from "@coinbase/wallet-sdk";
-require('dotenv').config();
+// require('dotenv').config();
 
 export const providerOptions = {
     walletlink: {
         package: CoinbaseWalletSDK, // requried
         options: {
             appName: "Web 3 Modal Example", // required
-            infuraId: process.env.INFURA_ID
+            infuraId: process.env.REACT_APP_INFURA_ID
         }
     },
     walletconnect: {
         package: WalletConnect, // required
         options: {
-            infuraId: process.env.INFURA_ID
+            infuraId: process.env.REACT_APP_INFURA_ID
         }
     }
 }
