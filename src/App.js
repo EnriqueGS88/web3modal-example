f534dcad65357092b6cca67a42bad98777e4e6b6
EnriqueGS88 <enrique.gzs@gmail.com>
2022-08-27 23:40:21 +0200

    added ethers and web3modal packages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
 // import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+import {
+  VStack,
+  Button,
+  Text,
+  HStack,
+  Select,
+  Input,
+  Box,
+  Tooltip
+} from '@chakra-ui/react';
+import { CheckCircleIcon, WarningIcon } from '@chakra-ui/icon'
+import { networkParams } from './networks';
+import { toHex, truncateAddress } from './utils';
+import { ethers } from 'ethers';
+import Web3Modal from 'web3modal';
+// import { providerOptions } from './providerOptions';
+
+
 
 function App() {
   return (
--- src/utils.js
+++ src/utils.js
@@ -7,7 +7,7 @@ export const truncateAddress = (address) => {
     return `${match[1]}…${match[2]}`;
 };
   
-  export const toHex = (num) => {
+export const toHex = (num) => {
 const val = Number(num);
 return "0x" + val.toString(16);
 };
